1be84e9e78cf15586d1466d852914993f802f1ca
Nick Playfair <nick@playfair.dev>
2021-01-22 00:34:29 +0000

    Basic task components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Have breakfast',
+      day: 'Feb 5th 08:00',
+      reminder: false
+    },
+    {
+      id: 2,
+      text: 'Feed cat',
+      day: 'Feb 5th 08:30',
+      reminder: true
+    },
+    {
+      id: 3,
+      text: 'Post letters',
+      day: 'Feb 5th 10:00',
+      reminder: true
+    }
+  ])
+
+  // Delete task
+  const deleteTask = (id) => {
+    console.log('delete ', id)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Header title='Task Tracker'/>
+      <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
   );
 }
